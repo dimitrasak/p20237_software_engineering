468cf5dff68d56bc379c77598c3d8f858dda1d18
dimitrasak <dimitrasak02@gmail.com>
2024-10-25 19:10:38 +0300

    software eng 1st commit

---
+++ src/main/java/com/unipi/sakellariou/p20237/P20237Application.java
@@ -0,0 +1,13 @@
+package com.unipi.sakellariou.p20237;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P20237Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P20237Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/unipi/sakellariou/p20237/P20237ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipi.sakellariou.p20237;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P20237ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
